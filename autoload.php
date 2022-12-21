9f83aae34cecadb28944a8414a596f13ac115f76
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-12-21 08:08:53 -0300

    register new function to load Cadastro and Cliente classes

--- autoload.php
+++ autoload.php
@@ -1,12 +1,21 @@
 <?php
     define('CLASSES', $_SERVER['DOCUMENT_ROOT'] . '/exercicios/classes/');
+    define('CLIENTE', $_SERVER['DOCUMENT_ROOT'] . '/exercicios/classes/cliente');
     
     spl_autoload_register('loadClasses');
+    spl_autoload_register('loadCliente');
 
     function loadClasses($class){
         set_include_path(CLASSES);
         if(file_exists(CLASSES . "$class.php")){
             require_once(CLASSES . "$class.php");
+        }
+    }
+
+    function loadCliente($class){
+        set_include_path(CLIENTE);
+        if(file_exists(CLIENTE . "$class.php")){
+            require_once(CLIENTE . "$class.php");
         }
     }
 ?>
