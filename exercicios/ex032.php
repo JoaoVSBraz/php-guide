8630badd2af844da088fe763c97e6a779da48fa8
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-12-28 15:28:10 -0300

    add exercise image

--- exercicios/ex032.php
+++ exercicios/ex032.php
@@ -5,7 +5,7 @@
             <div class="content">
             <p>O exercício abaixo executa uma das operações fundamentais em bancos de dados: <strong>UPDATE</strong>.</p>
 
-                <p><img src="" alt=""></p>
+                <p><img src="../assets/images/ex032.png" alt="Exemplo do comando sql UPDATE"></p>
             </div>
             <div class="result">
                 <h2>Saída</h2>
