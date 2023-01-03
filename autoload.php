9214ecab5d8acb56400d1fd017cbd599543f4759
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-03 10:07:37 -0300

    add DAO classes autoloader to autoload

--- autoload.php
+++ autoload.php
@@ -1,9 +1,11 @@
 <?php
     define('CLASSES', $_SERVER['DOCUMENT_ROOT'] . '/exercicios/classes/');
     define('CLIENTE', $_SERVER['DOCUMENT_ROOT'] . '/exercicios/classes/cliente');
+    define('DAO', $_SERVER['DOCUMENT_ROOT'] . '/exercicios/classes/dao/');
     
     spl_autoload_register('loadClasses');
     spl_autoload_register('loadCliente');
+    spl_autoload_register('loadDAO');
 
     function loadClasses($class){
         set_include_path(CLASSES);
@@ -16,6 +18,13 @@
         set_include_path(CLIENTE);
         if(file_exists(CLIENTE . "$class.php")){
             require_once(CLIENTE . "$class.php");
+        }
+    }
+
+    function loadDAO($class){
+        set_include_path(DAO);
+        if(file_exists(DAO . "$class.php")){
+            require_once(DAO . "$class.php");
         }
     }
 ?>
