79438f63f8ceef82ae1fa5bed04367d14a848672
JoaoVSBraz <joaovictor3.14@outlook.com>
2023-01-31 10:58:32 -0300

    update ex038 with description and example images

--- exercicios/ex038.php
+++ exercicios/ex038.php
@@ -4,9 +4,13 @@
         <h1>DAO - Insert</h1>
         <section>
             <div class="content">
-                <p></p>
+                <p>Este exercício consiste na criação de uma <strong>Stored Procedure</strong> e do método <code>insert()</code> na classe <code>User</code> a fim de <strong>inserir</strong> um usuário no banco de dados. Store Procedure significa <strong>Procedimento Armazenado</strong> e define um conjunto de comandos SQL executados de uma só vez. Funciona exatamente como uma função só que no contexto de Banco de Dados.</p>
 
-                <p><img src="" alt=""></p>
+                <p>A procedure criada foi a <code>sp_user_insert</code>. Para executá-la, no contexto do MySQL,utiliza-se o comando <code>CALL</code> seguido do nome da procedure.</p>
+
+                <p><img src="../assets/images/sp_user_insert.png" alt="Código referente a stored procedure que insere um usuário no banco de dados"></p>
+
+                <p><img src="../assets/images/ex038.png" alt="Código da função insert que executa a stored procedure"></p>
             </div>
             <div class="result">
                 <h2>Saída</h2>
